ae8b02c63cb6629f0a8e3e7cf04f23f11c969c55
likeRudin <likeRudin@gmail.com>
2023-09-04 22:48:42 +0900

    change firebase config env for vite form

---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+    VITE_API_KEY: string,
+    VITE_AUTH_DOMAINVITE_PROJECT_ID: string,
+    VITE_STORAGE_BUCKET: string,
+    VITE_MESSAGING_SENDER_ID: string,
+    VITE_APP_ID: string
+}
