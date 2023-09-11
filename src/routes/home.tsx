37ff65ed01c1068328d6128ffd3a1b8c6210351f
likeRudin <likeRudin@gmail.com>
2023-09-11 20:48:19 +0900

    add LogoutButton on Home

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
+import {auth} from "@/firebase.config";
+
+const Button = styled.button`
+width: 250px;
+height: 80px;
+`
+
 export const Home = () => {
+    const logout = () => {
+        auth.signOut();
+    }
     return (
         <>
             <span> "this is home"</span>
+            <Button onClick={logout}>Log Out!</Button>
         </>
     )
 }
